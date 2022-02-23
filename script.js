9de569f49686e0b71117845def03037e146be5f5
PrakatNeupane <60876051+PrakatNeupane@users.noreply.github.com>
2022-02-24 06:35:58 +1100

    final features

--- script.js
+++ script.js
@@ -53,9 +53,20 @@ class Calculator {
     }
 
     getDisplayNumber(number) {
-        const floatNumber = parseFloat(number)
-        if (isNan(floatNumber)) return ''
-        return floatNumber.toLocaleString('en')
+        const stringNumber = number.toString();
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {
+                maximumFractionDigits: 0
+            })
+        } if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else return integerDisplay
+
     }
 
     updateDisplay() {
@@ -63,7 +74,7 @@ class Calculator {
         if (this.operation != null) {
             this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
 
-        }
+        } else { this.previousOperandTextElement.innerText = '' }
     }
 }
 
